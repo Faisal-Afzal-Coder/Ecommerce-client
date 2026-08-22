89fb4ee02e0e6f8952f641238a040ccc34da856c
Faisal-Afzal-Coder <faisalafzal0008@gmail.com>
2026-08-22 12:26:38 -0700

    .env file created

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
@@ -7,6 +8,8 @@ import './index.css';
 import { AuthProvider } from './context/AuthContext';
 import { CartProvider } from './context/CartContext';
 import { StoreProvider } from './context/StoreContext';
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || '';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
